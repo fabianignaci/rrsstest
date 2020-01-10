240f003547918839deecd2dcef0be25f7b19eeb3
Fabian Toro <fabian.toro.ri@gmail.com>
2020-01-10 18:07:16 -0300

    Se agrega pl-2 al Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,7 +6,7 @@ import instaLogo from "../assets/static/insta-logo.png";
 import "../assets/styles/Header.css";
 
 const Header = () => (
-  <header className='container-fluid bg-custom'>
+  <header className='container-fluid bg-custom py-2'>
     <div className='text-white d-flex'>
       <div className='mx-auto'>
         <img src={instaLogo} alt='' />
