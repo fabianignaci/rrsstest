f2a819c26f6de56b89d239d6716b7bf02ecc060d
Fabian Toro <fabian.toro.ri@gmail.com>
2020-01-10 21:34:16 -0300

    Se agrega loader css

---
+++ src/components/Loader.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "../assets/styles/Loader.css";
+
+const Loader = () => (
+  <div class='lds-default'>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+    <div></div>
+  </div>
+);
+
+export default Loader;
